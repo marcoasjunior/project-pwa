436a17eaecc3807862c095b0dc9e07b8b5090201
Marco <marcoasjunior90@gmail.com>
2019-10-23 01:06:10 -0300

    Pronto para ser modificado
    Pedro

--- backend/express.js
+++ backend/express.js
@@ -3,20 +3,18 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const {
-    User
-} = require('./models');
+const {User} = require('./models');
 
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(bodyParser.json());
 
-User.create({
-    name: 'Marco',
-    email: 'marco@rocketseat.com.br',
-    password: '123456'
-});
+// User.create({
+//     name: 'Pedro',
+//     email: 'pedro@rocketseat.com.br',
+//     password: '123456'
+// });
 
 app.listen(3000, (req, res) => {
     console.log('Rodando')
