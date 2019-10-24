45e926c65b8f2587365504902c2383e9927094b4
Pedro lopes <pedrolopeshls99@gmail.com>
2019-10-24 08:58:06 -0300

    página inicial do app e componentes
    (deve ser feita correção no arquivo App.vue)

---
+++ src/views/principal.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <svg id="Camada_1" data-name="Camada 1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 478.39 579.33"><defs><style>.cls-1{fill:url(#Gradiente_sem_nome_4);}.cls-2{fill:url(#Gradiente_sem_nome_4-2);}.cls-3{fill:url(#Gradiente_sem_nome_4-3);}.cls-4{fill:url(#Gradiente_sem_nome_4-4);}</style><linearGradient id="Gradiente_sem_nome_4" x1="2.75" y1="1078.94" x2="6.4" y2="1075.45" gradientTransform="matrix(100.99, 0, 0, -99.81, 439.05, 108155.36)" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#f9a538"/><stop offset="0.17" stop-color="#f9a538"/><stop offset="1" stop-color="#ff4f94"/></linearGradient><linearGradient id="Gradiente_sem_nome_4-2" x1="16.25" y1="1076.04" x2="19.91" y2="1072.55" gradientTransform="matrix(19.47, 0, 0, -38.2, 548.47, 41578.91)" xlink:href="#Gradiente_sem_nome_4"/><linearGradient id="Gradiente_sem_nome_4-3" x1="20.54" y1="1074.51" x2="24.2" y2="1071.02" gradientTransform="matrix(19.47, 0, 0, -28.77, 572.33, 31390.9)" xlink:href="#Gradiente_sem_nome_4"/><linearGradient id="Gradiente_sem_nome_4-4" x1="25.44" y1="1080.4" x2="29.1" y2="1076.91" gradientTransform="matrix(19.33, 0, 0, -79.57, 595.67, 86259.21)" xlink:href="#Gradiente_sem_nome_4"/></defs><title>logo</title><g id="Grupo_269" data-name="Grupo 269"><path id="Caminho_6" data-name="Caminho 6" class="cls-1" d="M724.65,416.61S708.12,372.5,756.09,356c0,0,44.1-8.27,48,30.31,0,0,24.82,107.53,30.88,134,0,0,4.4,72.77,10.48,87.66a67.63,67.63,0,0,0,39.69,48.89s21,33.07,44.11,40.8c0,0,95.56,6.23,106.22,7,0,0,18.39,14.34,22.43,23.53,0,0,3.31-50.72-29.41-52.19l-71.67-4s-68.37,1.1-52.58-69.1c0,0,104-5.51,175.7-.37a243.8,243.8,0,0,1,119.45,71.69s6.07,57.31-12.67,93.16c0,0-43.58,83.26-210.62,50.73,0,0-61.58,16-110.82,7.54,0,0-80.86-1.1-108.8-84.91,0,0,1.48-144.08-3.68-184.51C752.78,556.11,734,441.79,724.65,416.61Z" transform="translate(-721.9 -248.67)"/><path id="Caminho_7" data-name="Caminho 7" class="cls-2" d="M886.57,637.7s-1.55-30.32,0-36.67c0,0,1.56-16.54,25.82-18.47l33.9-1.38s.28-67.27-3-77.21c0,0,4.13-51.83-45.21-46.87,0,0-38-2.21-43,40.26,0,0-2.21,47.69,0,62C855.06,559.39,857.17,630,886.57,637.7Z" transform="translate(-721.9 -248.67)"/><path id="Caminho_8" data-name="Caminho 8" class="cls-3" d="M971.11,585h88.21s.28-72.06-3.86-93c0,0,1.66-45.67-44.38-43.19,0,0-41.07-.54-43.83,43C967.24,491.8,965.87,566.75,971.11,585Z" transform="translate(-721.9 -248.67)"/><path id="Caminho_9" data-name="Caminho 9" class="cls-4" d="M1077,582s59,14.34,91.52,43.58c0,0-13.23-110.84-5.52-162.09,0,0,6.07-178.11,5.52-183.6,0,0,.55-27.57-37-30.88,0,0-33.63-5.51-42.46,27.57Z" transform="translate(-721.9 -248.67)"/></g></svg>
+  </div>
+</template>
+
+<script>
+import cpmPrincipal from '../components/cpmPrincipal'
+export default {
+  name: 'home',
+  components: {
+    card
+  }
+}
+</script>
