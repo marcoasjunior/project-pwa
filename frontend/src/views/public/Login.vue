386b3bf0fb5c2c0263bad7380671e3b32ff8d1cf
Pedro lopes <pedrolopeshls99@gmail.com>
2019-11-03 22:26:46 -0300

    conecting with frontend

--- frontend/src/views/public/Login.vue
+++ frontend/src/views/public/Login.vue
@@ -14,7 +14,8 @@
       
         <div class="col-12 m0">
             <!-- <router-link to="/Feed">  -->
-            <cButton :cData="{type:'submit', text:'Entrar',link:'SucessLogin',class:' wp-btn-1 bg-gdr-1',actionType:'Path'}"></cButton>
+            <!-- <cButton :cData="{type:'submit', text:'Entrar',link:'SucessLogin',class:' wp-btn-1 bg-gdr-1',actionType:'Path'}" @click="checkUser()"></cButton> -->
+            <cButton :cData="{type:'submit', text:'Entrar',class:' wp-btn-1 bg-gdr-1',actionType:'Path'}" @click="checkUser()"></cButton>
              <!-- </router-link> -->
         </div>
     </div>
@@ -27,6 +28,7 @@ import topBar from '../../components/topBar.vue'
 import cButton from '../../components/cpmButton.vue'
 // import buttons from '@/components/buttons.vue'
 // import buttonZ from '@/components/button.vue'
+import {axios} from '../../main'
 
 export default {
   name: 'Login',
@@ -38,12 +40,25 @@ export default {
   },
   created() {},
         mounted() {
-
         },
         data: () => ({
           disable:{login:false , signup:true}
         }),
         methods: {
+
+          checkUser(){
+
+            // alert("ok")
+            // console.log("ok")
+
+            axios
+              .get('http://localhost:3000/api/register/user')
+              // .then(response => this.info = response.data)
+              // .catch(e => {
+              //   this.errors.push(e)
+            // })
+          }
+
         },
         computed: {
         
