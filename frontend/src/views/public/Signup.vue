0ae7be471b06484d748fba8feac489b543a94ec5
PHW <pedrolopeshls99@gmail.com>
2019-11-04 12:54:53 -0300

    POST create user, working

--- frontend/src/views/public/Signup.vue
+++ frontend/src/views/public/Signup.vue
@@ -5,18 +5,31 @@
     <div class="container-text-login">
       <h1>Cadastrar</h1>
       <p> Faça o cadastro com seu e-mail e senha</p>
+
+
+      <span>{{ data }} </span>
     </div>
 
-    <input type="text" class="input mt-5" placeholder="e-mail">
-    <input type="text" class="input mt-5" placeholder="senha">
-    <input type="text" class="input mt-5" placeholder="senha">
+
+    <input type="email" class="input mt-5" placeholder="e-mail" id="email" v-model="data.email" @keyup="callValidation()">
+    <input type="password" class="input mt-5" placeholder="senha"  v-model="data.password" @keyup="callValidation()">
+    <input type="password" class="input mt-5" placeholder="senha"  v-model="data.passwordConfirm" @keyup="callValidation()">
+
+    <br>
+    {{error}}
+    <br>
+    <!-- {{dataValid}} -->
+
+ 
 
     <div class="wp-checkbox">
-        <input type="checkbox" name="" id=""> Concordo com os termos e Condições
+        <input type="checkbox" v-model="dataValid.chekBox" @click="getStatusCheckBox()"> Concordo com os termos e Condições
     </div>
 
         <div class="col-12 m0">
-            <cButton :cData="{type:'submit', text:'Cadastrar',link:'feed',class:' wp-btn-1 bg-gdr-1'}"></cButton>
+          <span @click="getInfoInputs()">
+             <cButton :cData="{type:'submit', text:'Cadastrar',link:'feed',class:'wp-btn-1 bg-gdr-1'}" ></cButton>
+          </span>
         </div>
 
 
@@ -31,9 +44,13 @@ import cButton from '../../components/cpmButton.vue'
 import topBar from '../../components/topBar.vue'
 // import buttons from '@/components/buttons.vue'
 // import buttonZ from '@/components/button.vue'
+import axios from 'axios'
+
 
 export default {
   name: 'Login',
+  // props: ['testData'],
+
   components: {
     cButton,
     topBar
@@ -44,11 +61,104 @@ export default {
 
         },
         data: () => ({
-          disable:{login:false , signup:true}
+          disable:{login:false , signup:true},
+          data:{email:'',password:'',passwordConfirm:''},
+          formValid: false,
+          dataValid:{email:false,password:false,passwordConfirm:false,chekBox:''},
+          error: '',
         }),
         methods: {
-        },
-        computed: {
+          getInfoInputs(){
+            axios.post('http://localhost:3000/api/register/user' ,this.data, {
+            email: this.data.email,
+            password: this.data.password,
+            passwordConfirm: this.data.passwordConfirm
+            })
+          },
+          getStatusCheckBox(){
+            // this.dataValid.chekBox = true;
+            if(this.dataValid.chekBox == true){
+              // alert("box não selecionada")
+              } 
+            
+            if(this.dataValid.chekBox == false){
+              // alert("check box selecionada")
+              }
+          },
+
+
+          callValidation(){
+              this.formValid = true;
+              if(this.formValid){
+              this.formValid = this.checkEmailValid();
+              }
+              if(this.formValid){
+              this.formValid = this.checkPasswordValid();
+              }
+              if(this.formValid){
+      
+                this.formValid = this.checkPasswordConfirm();
+              }
+              if(this.formValid){
+                this.error = '';
+              }
+
+          },
+
+          checkEmailValid(){
+            if(!this.data.email){
+              return true
+            }
+            else if(this.data.email.length < 3){
+              this.error = "Email is not valid";
+              return false
+       
+            }else{
+              return true
+            }
+
+          },
+
+          checkPasswordValid(){
+              if(this.data.password.length < 1 || !this.data.password){
+                this.error = "Password too short";
+                return false
+              }else{
+                return true
+              }
+          },
+          checkPasswordConfirm(){
+
+            if(this.data.password != this.data.passwordConfirm){
+              this.error = "password does not match";
+                return false
+            }else{
+              return true;
+            }
+          },
+
+
+          // checkEmailValidKeyup(){
+          //   if(this.data.email == ''){
+          //     this.dataValid.email = false;
+          //   }else{
+          //        this.dataValid.email = true;
+          //   }
+          // },
+
+          // checkValidinputandstuff(){
+          //   if( this.dataValid.email && this.dataValid.passwordConfirm){
+          //     this.callAPIfunction();
+          //   }else{
+          //     this.showError();
+          //   }
+          // }
+
+
+
+
+      },
+       computed: {
         
         },
         watch: {}
--- frontend/src/views/public/Test.vue
+++ frontend/src/views/public/Test.vue
@@ -1,21 +1,47 @@
 <template>
   <div class="home">
-      ahdfpashfpha
+      <span>{{ datas }}</span>
    </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import {axios} from '../../main'
-// import axios from '../../main'
+import axios from 'axios'
+
+// import { http } from '../../main'
 
 export default {
-  name: 'Test',
-    data() {},
+    name: 'test',
+    
+    data() {
+        return {
+            info: null,
+            datas: []
+        }
+    }
+    ,
+    
     components: {},
-    created() {},
-    mounted() {},
-    methods: {},
+
+    created() {
+        this.getData();
+    },
+
+    mounted() {
+
+    },
+    methods: {
+
+        getData(){
+
+        axios.get('http://localhost:3000/api/list/user')
+        .then(response => this.datas = response.data)
+        // .catch(e => {
+            // this.errors.push(e)
+        // })
+        }
+    },
     computed: {},
     watch: {}
 }
