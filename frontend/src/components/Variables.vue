f6d75690b3b20ec0a2f879af74f6339665edb43e
Pedro <pedrolopeshls99@gmail.com>
2019-11-04 21:24:37 -0200

    componente com variáveis de rota.

---
+++ frontend/src/components/Variables.vue
@@ -0,0 +1,31 @@
+<template>
+<div>  
+   
+</div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'cpmButton',
+
+  components: {
+
+  },
+  created() {},
+        mounted() {
+    
+        },
+        data: () => ({
+
+        }),
+        methods: {
+    
+        },
+        computed: {
+        
+        },
+        watch: {}
+}
+</script>
