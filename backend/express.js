b664b155057bc144c1a9dcec7734f6ee78926e70
Pedro lopes <pedrolopeshls99@gmail.com>
2019-11-05 20:34:48 -0300

    update

--- backend/express.js
+++ backend/express.js
@@ -2,6 +2,8 @@ const app = require('express')()
 const bodyParser = require('body-parser')
 const router = require('./routes/router')
 const cors = require('cors')
+var path = require('path')
+var serveStatic = require('serve-static')
 
 
 //SOMENTE USAR PARA CRIAR TABELAS
@@ -14,10 +16,16 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded())
 app.use('/api', router)
+app.use(serveStatic(path.join(__dirname, 'dist')))
 
 
 // eslint-disable-next-line no-unused-vars
-app.listen(3000, (req, res) => {
-    // eslint-disable-next-line no-console
-    console.log('Rodando')
-})
+// app.listen(3000, (req, res) => {
+//     // eslint-disable-next-line no-console
+//     console.log('Rodando')
+// })
+
+
+var port = process.env.PORT || 3000
+app.listen(port)
+console.log('ta Rolando ' + port)
