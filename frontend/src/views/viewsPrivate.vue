b9ad26d3dda084bab06ac5002f823be99cdfa1ef
Marco <marcoasjunior90@gmail.com>
2019-11-13 23:20:34 -0300

    feita animação de rotas

--- frontend/src/views/viewsPrivate.vue
+++ frontend/src/views/viewsPrivate.vue
@@ -2,12 +2,15 @@
     <div>
 
         <ul class="navbar sticky-top shadow-sm p-3 mb-5 bg-white rounded" role="navigation" id="ul">
-            <router-link id="bt_rota" v-for="tab in tabs" :key='tab.id' :to='tab.path' class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" role="presentation">
+            <router-link id="bt_rota" v-for="tab in tabs" :key='tab.id' :to='tab.path'
+                class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" role="presentation">
                 {{tab.name}}
             </router-link>
         </ul>
 
-        <router-view></router-view>
+      <transition name="router-anim">
+   <router-view/>
+</transition>
 
     </div>
 </template>
@@ -64,6 +67,35 @@ a:visited {
 
 a:hover {
     text-decoration: none;
+}
+
+.router-anim-enter-active {
+  animation: coming 1s;
+  animation-delay: .5s;
+  opacity: 0;
+}
+.router-anim-leave-active {
+  animation: going .2s;
+}
+
+@keyframes going {
+  from {
+    transform: translateX(0);
+  }
+  to {
+    transform: translateX(-30px);
+    opacity: 0;
+  }
+}
+@keyframes coming {
+  from {
+    transform: translateX(-30px);
+    opacity: 0;
+  }
+  to {
+    transform: translateX(0);
+    opacity: 1;
+  }
 }
 
 </style>
