6285e04e95d9028771165dcd19c621f99d2cb4c7
PHW <pedrolopeshls99@gmail.com>
2019-11-20 10:46:18 -0300

    pequenos fixs

--- frontend/src/components/fakeCard.vue
+++ frontend/src/components/fakeCard.vue
@@ -1,14 +1,14 @@
 <template>
   
     
-      <div class="card shadow rounded">
+      <div class="fake-card shadow rounded">
         <b-img src="https://media.giphy.com/media/3o7aTskHEUdgCQAXde/giphy.gif" fluid alt="Responsive image"></b-img>
         <div class="card-body">
           <h4 class="card-title">Desculpe, mas não encontramos nenhum evento! Tente novamente mais tarde</h4>
           <hr>
           <p class="card-text"> <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="map-marker-alt"
               role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"
-              class="svg-inline--fa fa-map-marker-alt fa-w-12 fa-3x">
+              class="icon-location">
               <path fill="currentColor"
                 d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"
                 class=""></path>
