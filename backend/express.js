75ea3fd84d4549e8d32b0bfa0590f30ac460023b
Marco <marcoasjunior90@gmail.com>
2019-11-28 01:04:00 -0300

    alterar express para tudo virar HTTPS

--- backend/express.js
+++ backend/express.js
@@ -12,12 +12,22 @@ const port = process.env.PORT || 3000
 // Models.sequelize.sync({ force: true })
 
 app.use(cors())
+app.use (function (req, res, next) {
+  if (req.secure) {
+          // request was via https, so do no special handling
+          next();
+  } else {
+          // request was via http, so redirect to https
+          res.redirect('https://' + req.headers.host + req.url);
+  }
+});
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended: true
   }))
 app.use('/api', router)
 app.use(serveStatic(path.join(__dirname, '../dist')))
+
 
 app.listen(port)
 
