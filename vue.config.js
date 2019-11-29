6bfc969e0f3a311687151e735363e7c77031832d
Marco <marcoasjunior90@gmail.com>
2019-11-28 21:06:26 -0300

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 // ALTERAR EM CASO DE PRODUÇÃO OU LOCAL
 
 
-// process.env.PROD_API= 'https://weparty-app.herokuapp.com/api'
-process.env.PROD_API= 'http://localhost:8080/api'
+process.env.PROD_API= 'https://weparty-app.herokuapp.com/api'
+// process.env.PROD_API= 'http://localhost:8080/api'
 
 
 module.exports = {
