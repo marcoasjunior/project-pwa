fd0de979fc65415267cd009fdaf45bc20056dbff
Marco <marcoasjunior90@gmail.com>
2019-12-01 20:51:06 -0300

    trYYYYYYYYYYYY

--- backend/express.js
+++ backend/express.js
@@ -17,15 +17,15 @@ app.use(cors())
 
 // ATIVAR SOMENTE QUANDO FOR PARA PRODUÇÃO
 
-app.use (function (req, res, next) {
-  if (req.secure) {
-          // request was via https, so do no special handling
-          next();
-  } else {
-          // request was via http, so redirect to https
-          res.redirect('https://' + req.headers.host + req.url);
-  }
-});
+// app.use (function (req, res, next) {
+//   if (req.secure) {
+//           // request was via https, so do no special handling
+//           next();
+//   } else {
+//           // request was via http, so redirect to https
+//           res.redirect('https://' + req.headers.host + req.url);
+//   }
+// });
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended: true
