62a3bef07c0c397f246c85ece0b5be090d50ad40
Marco <marcoasjunior90@gmail.com>
2019-12-03 00:18:52 -0300

    SA DEPLOY

--- backend/express.js
+++ backend/express.js
@@ -6,6 +6,19 @@ const path = require('path')
 const serveStatic = require('serve-static')
 
 const port = process.env.PORT || 3000
+var https_redirect = function(req, res, next) {
+  if (process.env.NODE_ENV === 'production') {
+      if (req.headers['x-forwarded-proto'] != 'https') {
+          return res.redirect('https://' + req.headers.host + req.url);
+      } else {
+          return next();
+      }
+  } else {
+      return next();
+  }
+};
+
+app.use(https_redirect);
 
 //SOMENTE USAR PARA CRIAR TABELAS
 
